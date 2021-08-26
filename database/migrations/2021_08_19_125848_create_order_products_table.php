4e4b3ca4f887e9141e0997a33ddd1fff86ef52f2
andrii <cord811@hotmail.com>
2021-08-26 21:22:06 +0300

    first commit

---
+++ database/migrations/2021_08_19_125848_create_order_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->bigInteger('order_id');
+            $table->bigInteger('product_id');
+            $table->enum('status', ['new', 'in_progress', 'complete', 'delivered'])   ->default('new');
+            $table->integer('qty');
+            $table->primary(['order_id', 'product_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_products');
+    }
+}
